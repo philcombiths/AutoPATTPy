37eddab45343205f36262b5d7bf2f384b1700c3b
philcombiths <philcombiths@gmail.com>
2020-09-08 17:09:39 -0700

    Update file_rename.py
    Added costumizability to file_rename fx

--- AutoPATTPy/file_rename.py
+++ AutoPATTPy/file_rename.py
@@ -8,19 +8,40 @@ import os
 from AutoPATTPy import change_dir
 
 
-def file_rename(directory):
-    """Truncates filenames in directory up to first "_" only, preserving extension."""
+def file_rename(directory, filetypes=None, filetypes_exclude=('.ini'), 
+                filename_splitter='_', truncate_index=0):
+    """
+    Truncates filenames, preserving extension.
+    
+    Parameters:
+        directory : directory of files to be renamed
+        filetypes : str or tuple of extensions to be included in renaming (optional).
+        filetypes_excluded : str or tuple of extensions to be excluded in renaming. Default = '.ini'
+        filename_splitter : str of splitter to use for truncating.
+        truncate_index : int index of splitter to use for truncating.
+    """    
     with change_dir(directory):
         for file in os.listdir(directory):
-            if file.endswith('.csv'):
-                if '_' in file:
-                    file_rev = f"{file.split('_')[0]}.{file.split('.')[-1]}"
+            # Skip excluded filetypes
+            if file.endswith(filetypes_exclude):
+                continue
+            # Rename all files if filetypes not specified
+            if filetypes==None:
+                if filename_splitter in file:
+                    file_rev = f"{file.split('_')[truncate_index]}.{file.split('.')[-1]}"
                     print(f"{file} --> {file_rev}")
-                    os.rename(file, file_rev)
+                    os.rename(file, file_rev)                
+            # Rename only filetypes specified
+            else:                
+                if file.endswith(filetypes):
+                    if filename_splitter in file:
+                        file_rev = f"{file.split('_')[truncate_index]}.{file.split('.')[-1]}"
+                        print(f"{file} --> {file_rev}")
+                        os.rename(file, file_rev)
     print('Done.')
     return
 
 
 if __name__=='__main__':
     directory = r'G:\My Drive\Phonological Typologies Lab\Projects\AutoPATT\Manual PATT Validation\AutoPATT Data'
-    file_rename(directory)    
+    file_rename(directory, '.csv')
