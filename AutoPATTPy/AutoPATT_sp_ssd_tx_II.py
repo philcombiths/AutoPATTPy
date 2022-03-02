31b33bb776fe7f4d8ba8e2fd05cf272aa3f1d454
philcombiths <philcombiths@gmail.com>
2022-03-02 08:18:33 -0600

    Update AutoPATT_sp_ssd_tx_II.py

--- AutoPATTPy/AutoPATT_sp_ssd_tx_II.py
+++ AutoPATTPy/AutoPATT_sp_ssd_tx_II.py
@@ -31,7 +31,8 @@ def import_files_SpTx(directory):
             if f.endswith('.csv'):
                 ID = re.findall('C\d\d\d', f)[0]
                 phase = re.findall('Pre|Post', f)[0]
-                autopatt_objs[ID+phase] = AutoPATT(f, legacy=False)
+                lang = re.findall('Spanish|English', f)[0]
+                autopatt_objs[ID+phase+lang] = AutoPATT(f, legacy=False)
     return autopatt_objs
 
 
@@ -42,9 +43,11 @@ def compare_all_SpTx(directory):
     
     data = import_files_SpTx(directory)
     for variable in ['phonetic_inv', 'phonemic_inv', 'cluster_inv']:
-        data['C101Pre'].compare(data['C101Post'], variable)
-        data['C102Pre'].compare(data['C102Post'], variable)
+        data['C101PreEnglish'].compare(data['C101PostEnglish'], variable)
+        data['C102PreEnglish'].compare(data['C102PostEnglish'], variable)
+        data['C101PreSpanish'].compare(data['C101PostSpanish'], variable)
+        data['C102PreSpanish'].compare(data['C102PostSpanish'], variable)
     return data
 
-directory = r'C:\Users\Philip\University of Iowa\Clinical Linguistics and Disparities Lab - Documents\CLD Lab\Projects\Spanish SSD Tx\Summer 2021\data\autoPATT\test'
+directory = r'D:\Data\test'
 Sp_data = compare_all_SpTx(directory)
